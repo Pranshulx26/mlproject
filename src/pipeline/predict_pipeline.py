116f6b1511c8547305220260173ec8eeaf351af9
Pranshul Sharma <pranshulsharma83@gmail.com>
2024-10-27 19:01:10 +0530

    Prediction Pipeline

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,73 @@
+import sys 
+import pandas as pd 
+from src.exception import CustomException
+from src.utils import load_object 
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass 
+    
+    def predict(self, features):
+        """Predict the maths score based on the given features
+
+        Args:
+            features (dataframe): features for predicting math score
+
+        Returns:
+            float: return math score
+        """
+        try:
+            model_path='artifacts\model.pkl'
+            preprocessor_path='artifacts\preprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds 
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+class CustomData:
+    def __init__(self,
+                 gender: str,
+                 race_ethnicity: str,
+                 parental_level_of_education,
+                 lunch: str, 
+                 test_preparation_course: str,
+                 reading_score: int, 
+                 writing_score: int):
+        self.gender = gender 
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch 
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score 
+        self.writing_score = writing_score
+        
+    def get_data_as_data_frame(self):
+        """Convert the provided feature details from user into a dataframe
+
+        Raises:
+            CustomException: Exception Details
+
+        Returns:
+            DataFrame: returns a dataframe of provided feature values 
+        """
+        try:
+            custom_data_input_dict = {
+                'gender': [self.gender],
+                'race_ethnicity': [self.race_ethnicity],
+                'parental_level_of_education': [self.parental_level_of_education],
+                'lunch': [self.lunch],
+                'test_preparation_course': [self.test_preparation_course],
+                'reading_score': [self.reading_score],
+                'writing_score': [self.writing_score]
+                   
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+    
